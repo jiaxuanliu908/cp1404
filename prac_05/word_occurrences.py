1c402123874072ee7ce2f406d404cf338dd8e76c
jiaxuanliu908 <139741351+jiaxuanliu908@users.noreply.github.com>
2023-08-13 16:03:13 +0800

    Add files via upload

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,16 @@
+def word_count():
+    string = input("Enter a string: ")
+    words = string.split()
+    word_counts = {}
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+    max_word_length = max(len(word) for word in word_counts.keys())
+
+    for word, count in sorted(word_counts.items()):
+        print(f"{word:{max_word_length}} : {count}")
+
+word_count()
